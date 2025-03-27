f0710fbe264eeac359fc47fe2548c36415cde13a
Luiza Costa <mylenaluizajor@gmail.com>
2025-03-26 23:13:13 -0300

    Adjusting interfaces

--- Order.java
+++ Order.java
@@ -1,9 +1,7 @@
-import java.time.format.DateTimeFormatter;
-import java.util.Comparator;
+
 import java.util.List;
 
 public interface Order<Task> {
-    List<Task> orderTasks(Comparator<Task> comparator);
 
     List<Task> orderDeadline();
 
